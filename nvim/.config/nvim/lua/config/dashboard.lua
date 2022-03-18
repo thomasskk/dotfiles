1ab098a57781f6110b8faca78fd2d3a1650ff2f0
thomas <tkitengec@gmail.com>
2022-03-18 17:49:53 +0100

    dashboard

--- nvim/.config/nvim/lua/config/dashboard.lua
+++ nvim/.config/nvim/lua/config/dashboard.lua
@@ -2,15 +2,15 @@ local g = vim.g
 g.dashboard_session_directory = "~/.config/nvim/.sessions"
 g.dashboard_default_executive = "telescope"
 g.dashboard_custom_section = {
-	a = { description = { "  Find File                 " }, command = "Telescope find_files" },
-	b = { description = { "  Recents                   " }, command = "Telescope oldfiles" },
-	d = { description = { "  New File                  " }, command = "DashboardNewFile" },
-	e = { description = { "  Bookmarks                 " }, command = "Telescope marks" },
-	f = { description = { "  Load Last Session         " }, command = "SessionLoad" },
-	h = { description = { "  Settings                  " }, command = "edit $MYVIMRC" },
+	a = { description = { "  Find File        " }, command = "Telescope find_files" },
+	b = { description = { "  Recents          " }, command = "Telescope oldfiles" },
+	d = { description = { "  New File         " }, command = "DashboardNewFile" },
+	e = { description = { "  Bookmarks        " }, command = "Telescope marks" },
+	f = { description = { "  Load Last Session" }, command = "SessionLoad" },
+	h = { description = { "  Settings         " }, command = "edit $MYVIMRC" },
 }
 
-g.dashboard_custom_footer = { "type  :help<Enter>  or  <F1>  for on-line help" }
+g.dashboard_custom_footer = { "neovim" }
 vim.cmd([[
 augroup dashboard_au
      autocmd! * <buffer>
@@ -21,39 +21,15 @@ augroup dashboard_au
 augroup END
 ]])
 g.dashboard_custom_header = {
-	"            :h-                                  Nhy`               ",
-	"           -mh.                           h.    `Ndho               ",
-	"           hmh+                          oNm.   oNdhh               ",
-	"          `Nmhd`                        /NNmd  /NNhhd               ",
-	"          -NNhhy                      `hMNmmm`+NNdhhh               ",
-	"          .NNmhhs              ```....`..-:/./mNdhhh+               ",
-	"           mNNdhhh-     `.-::///+++////++//:--.`-/sd`               ",
-	"           oNNNdhhdo..://++//++++++/+++//++///++/-.`                ",
-	"      y.   `mNNNmhhhdy+/++++//+/////++//+++///++////-` `/oos:       ",
-	" .    Nmy:  :NNNNmhhhhdy+/++/+++///:.....--:////+++///:.`:s+        ",
-	" h-   dNmNmy oNNNNNdhhhhy:/+/+++/-         ---:/+++//++//.`         ",
-	" hd+` -NNNy`./dNNNNNhhhh+-://///    -+oo:`  ::-:+////++///:`        ",
-	" /Nmhs+oss-:++/dNNNmhho:--::///    /mmmmmo  ../-///++///////.       ",
-	"  oNNdhhhhhhhs//osso/:---:::///    /yyyyso  ..o+-//////////:/.      ",
-	"   /mNNNmdhhhh/://+///::://////     -:::- ..+sy+:////////::/:/.     ",
-	"     /hNNNdhhs--:/+++////++/////.      ..-/yhhs-/////////::/::/`    ",
-	"       .ooo+/-::::/+///////++++//-/ossyyhhhhs/:///////:::/::::/:    ",
-	"       -///:::::::////++///+++/////:/+ooo+/::///////.::://::---+`   ",
-	"       /////+//++++/////+////-..//////////::-:::--`.:///:---:::/:   ",
-	"       //+++//++++++////+++///::--                 .::::-------::   ",
-	"       :/++++///////////++++//////.                -:/:----::../-   ",
-	"       -/++++//++///+//////////////               .::::---:::-.+`   ",
-	"       `////////////////////////////:.            --::-----...-/    ",
-	"        -///://////////////////////::::-..      :-:-:-..-::.`.+`    ",
-	"         :/://///:///::://::://::::::/:::::::-:---::-.-....``/- -   ",
-	"           ::::://::://::::::::::::::----------..-:....`.../- -+oo/ ",
-	"            -/:::-:::::---://:-::-::::----::---.-.......`-/.      ``",
-	"           s-`::--:::------:////----:---.-:::...-.....`./:          ",
-	"          yMNy.`::-.--::..-dmmhhhs-..-.-.......`.....-/:`           ",
-	"         oMNNNh. `-::--...:NNNdhhh/.--.`..``.......:/-              ",
-	"        :dy+:`      .-::-..NNNhhd+``..`...````.-::-`                ",
-	"                        .-:mNdhh:.......--::::-`                    ",
-	"                           yNh/..------..`                          ",
-	"                                                                    ",
-	"                              N E O V I M                           ",
+    [[   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ]],
+    [[    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ]],
+    [[          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ]],
+    [[           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ]],
+    [[          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ]],
+    [[   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ]],
+    [[  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ]],
+    [[ ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ]],
+    [[ ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ]],
+    [[      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ]],
+    [[       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ]],
 }
