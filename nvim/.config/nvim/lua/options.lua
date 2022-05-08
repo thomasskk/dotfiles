f2bc660ff759738bb6aa8a13a7da7bfc3f07d0fe
thomasskk <tkitengec@gmail.com>
2022-05-08 23:24:57 +0200

    update

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -6,4 +6,12 @@ vim.cmd([[
 :  autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu && mode() != "i" | set rnu   | endif
 :  autocmd BufLeave,FocusLost,InsertEnter,WinLeave   * if &nu                  | set nornu | endif
 :augroup END
+:hi Normal guibg=none ctermbg=none
+:hi LineNr guibg=none ctermbg=none
+:hi Folded guibg=none ctermbg=none
+:hi NonText guibg=none ctermbg=none
+:hi SpecialKey guibg=none ctermbg=none
+:hi VertSplit guibg=none ctermbg=none
+:hi SignColumn guibg=none ctermbg=none
+:hi EndOfBuffer guibg=none ctermbg=none
 ]])
--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -2,11 +2,10 @@
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use
-# polybar-msg cmd quit
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar mybar 2>&1 | tee -a /tmp/polybar.log &
+polybar example 2>&1 | tee -a /tmp/polybar.log &
 disown
-
-echo "Polybar launched..."
