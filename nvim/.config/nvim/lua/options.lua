6a5a7a42885830549a5cb692ccc514e51887c091
thomas <tkitengec@gmail.com>
2022-05-09 17:17:07 +0200

    update

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -5,6 +5,7 @@ vim.cmd([[
 :  autocmd!
 :  autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu && mode() != "i" | set rnu   | endif
 :  autocmd BufLeave,FocusLost,InsertEnter,WinLeave   * if &nu                  | set nornu | endif
+:colorscheme gruvbox
 :augroup END
 :hi Normal guibg=none ctermbg=none
 :hi LineNr guibg=none ctermbg=none
