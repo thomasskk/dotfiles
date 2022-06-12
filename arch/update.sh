3260a4d59fa0d45010425094691e520a8063948e
thomasskk <tkitengec@gmail.com>
2022-06-12 23:40:50 +0200

    .

--- arch/update.sh
+++ arch/update.sh
@@ -4,8 +4,8 @@ cd ~/dotfiles || exit
 git fetch && git pull
 stow */ --adopt
 git reset --hard
-xargs -a ~/pkg.list yay -S --noconfirm
-xargs -a ~/npm.list sudo npm i --location=global
+xargs -a ~/pkg.list yay -S --noconfirm --needed
+xargs -a ~/npm.list sudo npm update --location=global
 source ~/.nvm/nvm.sh
 nvm install --lts
 bob use nightly
