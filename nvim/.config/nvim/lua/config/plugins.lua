3c9ca4980f09f388643a34fb9a591777493ecb34
thomasskk <tkitengec@gmail.com>
2022-12-04 23:44:23 +0100

    .

--- nvim/.config/nvim/lua/config/plugins.lua
+++ nvim/.config/nvim/lua/config/plugins.lua
@@ -40,6 +40,16 @@ return require("packer").startup({
 			},
 		})
 		use({
+			"folke/zen-mode.nvim",
+			config = function()
+				require("zen-mode").setup({
+					-- your configuration comes here
+					-- or leave it empty to use the default settings
+					-- refer to the configuration section below
+				})
+			end,
+		})
+		use({
 			"saecki/crates.nvim",
 			event = { "BufRead Cargo.toml" },
 			requires = { { "nvim-lua/plenary.nvim" } },
